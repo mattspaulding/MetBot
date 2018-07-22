cffe7445b2da62c17c333b892c36ee377de71732
Matt Spaulding <matthew.spaulding@iprocedures.com>
2018-07-22 11:47:37 -0400

    Add donate.

--- app.js
+++ app.js
@@ -393,7 +393,10 @@ function receivedPostback(event) {
     sendBotYesMessage(senderID);
   }else if(payload==='help'){
     sendHelpMessage(senderID);
-   
+    setTimeout(()=>{
+      sendDonateMessage(senderID);
+  
+    },10000)
   }else if(payload==='subscribeyes'){
     sendSubscribeConfirmMessage(senderID);
   }else{
